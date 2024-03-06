7fa9aede72b9f298e10bd9a669ecbeb2e3a0a393
Hannah Yu <realhannah.yu@mail.utoronto.ca>
2024-03-05 22:30:21 -0500

    trial

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -9,18 +9,30 @@
 
 
 #### Workspace setup ####
-library(opendatatoronto)
+library(dataverse)
 library(tidyverse)
 # [...UPDATE THIS...]
 
-#### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+ces2020 <-
+  get_dataframe_by_name(
+    filename = "CES20_Common_OUTPUT_vv.csv",
+    dataset = "10.7910/DVN/E9N6PH",
+    server = "dataverse.harvard.edu",
+    .f = read_csv
+  ) |>
+  select(votereg, CC20_410, race, region, CC20_300b_4, CC20_300b_5, CC20_303)
 
-
+write_csv(ces2020, "ces2020.csv")
+git lfs install
 
 #### Save data ####
 # [...UPDATE THIS...]
 # change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(raw_data.csv, "inputs/data/raw_data.csv") 
 
+git lfs track "path/to/large/file"
+git lfs track "data/raw_data/CES20_Common_OUTPUT_vv.csv"
+git lfs ls-files
+git push --force origin <your_branch_name>
+  
          
